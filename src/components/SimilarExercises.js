47030a45c7da6e185ace7b0a95a08213b0d58815
Utku Aktas <mattaktas94@gmail.com>
2022-10-10 16:58:11 -0400

    typo mucle to muscle

--- src/components/SimilarExercises.js
+++ src/components/SimilarExercises.js
@@ -7,7 +7,7 @@ const SimilarExercises = ({ targetMuscleExercises, equipmentExercises }) => {
   return (
     <Box sx={{ mt: { lg: "100px", xs: "0" } }}>
       <Typography variant="h3" mb={5}>
-        Exercises that target the same mucle group
+        Exercises that target the same muscle group
       </Typography>
       <Stack direction="row" sx={{ p: "2", position: "relative" }}>
         {targetMuscleExercises.length ? (
